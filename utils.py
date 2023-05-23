c613aa3fcb1ab1dc4747fec468125316a28d6ee9
WuShangbin <595606350@qq.com>
2023-05-23 22:45:55 +0800

    update region ID

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def get_taxi_zones_dic(shp_path):
     shp_df = geopandas.GeoDataFrame.from_file(shp_path)
     taxi_zone_shape = {}
     for i in range(len(shp_df)):
-        # Note: The current region_ID is not 0-179
+        # Note: The current region_ID is 0-179
         taxi_zone_shape[shp_df.iloc[i]['region_id']+1] = shp_df.iloc[i]['geometry']
     # print("Taxi Zone Number: ", len(taxi_zone_shape))
     return taxi_zone_shape
@@ -34,5 +34,3 @@ def get_zone_id(longitude, latitude):
 if __name__ == '__main__':
     print(get_zone_id("-73.9911117553711", "40.72775650024414"))
     print(get_zone_id(-73.99695587158203, 40.73704147338867))
-    print(geopandas.__version__)
-    print(shapely.__version__)
